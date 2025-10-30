97efa5da1c1208c35e713ab29253741c6be2411f
Vijay0908-lab <chauhanvijay0908@gmail.com>
2025-10-30 18:04:36 +0530

    learned about the MVC

--- routes/shop.js
+++ routes/shop.js
@@ -2,22 +2,12 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const productController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    // layout: false,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productController.getProducts);
+router.get("/products");
+router.get("/cart");
 
 module.exports = router;
