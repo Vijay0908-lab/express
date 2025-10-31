4d642216cc80cff52920beba4c1545f4c19ffd32
Vijay0908-lab <chauhanvijay0908@gmail.com>
2025-11-01 00:25:29 +0530

    learned more about the data storing

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
-const productController = require("../controllers/products");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", productController.getProducts);
-router.get("/products");
-router.get("/cart");
+router.get("/", shopController.getIndex);
+router.get("/products", shopController.getProducts);
+router.get("/cart", shopController.getCart);
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
