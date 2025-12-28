8c5ce4dff16abdd35875757be65d315b11f94788
Vijay0908-lab <chauhanvijay0908@gmail.com>
2025-12-29 02:31:48 +0530

    understanding of session and cookies

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "login",
+    isAuthenticated: isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("set-Cookie", "isLoggedIn=true");
+  res.redirect("/");
+};
