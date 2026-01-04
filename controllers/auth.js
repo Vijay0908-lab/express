e530fd611a1f5f52343acc9112f120609c634d28
Vijay0908-lab <chauhanvijay0908@gmail.com>
2026-01-05 00:50:14 +0530

    authentication using email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,43 +1,23 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
 const User = require("../models/user");
-const Nodemailer = require("nodemailer");
-require("dotenv").config();
 
-const transporter = Nodemailer.createTransport({
-  host: "email-smtp.us-east-1.amazonaws.com",
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
-  auth: {
-    user: process.env.user,
-    pass: process.env.pass,
+require("dotenv").config();
+const aws = require("@aws-sdk/client-sesv2");
+
+const ses = new aws.SESv2Client({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWSAccessKeyID,
+    secretAccessKey: process.env.AWSSecretKey,
   },
 });
 
-function createmailOption({ to, from, subject, html }) {
-  return {
-    from: from,
-    to: to,
-    subject: subject,
-    html: html,
-  };
-}
-
-function sendEmail({ to, from, subject, html }) {
-  const mailOption = createmailOption({ to, from, subject, html });
-  transporter.sendMail(mailOption, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent:", info.response);
-    }
-  });
-}
+const transport = nodemailer.createTransport({
+  ses: { ses, aws },
+});
 
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn =
-  //   req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
-  //console.log(req.session.user);
-  //console.log(req.session.isLoggedIn);
   let message = req.flash("error");
   if (message.length > 0) {
     message = message[0];
@@ -46,10 +26,11 @@ exports.getLogin = (req, res, next) => {
   }
   res.render("auth/login", {
     path: "/login",
-    pageTitle: "login",
+    pageTitle: "Login",
     errorMessage: message,
   });
 };
+
 exports.getSignup = (req, res, next) => {
   let message = req.flash("error");
   if (message.length > 0) {
@@ -57,73 +38,74 @@ exports.getSignup = (req, res, next) => {
   } else {
     message = null;
   }
-
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
   });
 };
+
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  User.findOne({ email: email, password: password })
+  User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash("error", "Invalid email or password");
+        req.flash("error", "Invalid email or password.");
         return res.redirect("/login");
       }
       bcrypt
         .compare(password, user.password)
         .then((doMatch) => {
           if (doMatch) {
-            req.session.user = user;
             req.session.isLoggedIn = true;
+            req.session.user = user;
             return req.session.save((err) => {
               console.log(err);
               res.redirect("/");
             });
-          } else {
-            req.flash("error", "Invalid email or password");
-            return res.redirect("/login");
           }
+          req.flash("error", "Invalid email or password.");
+          res.redirect("/login");
         })
-        //the above then dont cofirm that the password we are compare is actually match or not it return true whether is matched or not
         .catch((err) => {
-          console.log("error in authentication ", error);
+          console.log(err);
+          res.redirect("/login");
         });
     })
-    .catch((err) => {
-      console.log("error in my use function in admin.js", err);
-    });
+    .catch((err) => console.log(err));
 };
+
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
-        req.flash("error", "email already exist, Please pick different one");
+        req.flash(
+          "error",
+          "E-Mail exists already, please pick a different one."
+        );
         return res.redirect("/signup");
       }
       return bcrypt
         .hash(password, 12)
-        .then((hashPassword) => {
+        .then((hashedPassword) => {
           const user = new User({
             email: email,
-            password: hashPassword,
+            password: hashedPassword,
             cart: { items: [] },
           });
           return user.save();
         })
         .then((result) => {
           res.redirect("/login");
-          sendEmail({
+          console.log("about to send message");
+          return transport.sendMail({
             to: "chauhanvijay0908@gmail.com",
-            from: "chauhanvijay0908@gmail.com",
-            subject: "Signup Succeeded!",
+            from: "chouhanvijay0908@gmail.com",
+            subject: "Signup succeeded!",
             html: "<h1>You successfully signed up!</h1>",
           });
         })
@@ -131,14 +113,29 @@ exports.postSignup = (req, res, next) => {
           console.log(err);
         });
     })
-
     .catch((err) => {
       console.log(err);
     });
 };
+
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
   });
 };
